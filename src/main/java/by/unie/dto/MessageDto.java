4c2c5938913e592fb6c71ed631a8323d7e924607
Aliaksei Shkurko <shaleprog@gmail.com>
2022-10-09 17:49:30 +0300

    add @Data annotation

--- src/main/java/by/unie/dto/MessageDto.java
+++ src/main/java/by/unie/dto/MessageDto.java
@@ -1,10 +1,11 @@
 package by.unie.dto;
 
 import by.unie.chat.Message;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
+@Data
 @Component("messageDto")
 public class MessageDto {
     private String text;
@@ -17,30 +18,6 @@ public class MessageDto {
     public MessageDto(String text, String name, Date date) {
         this.text = text;
         this.name = name;
-        this.date = date;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
         this.date = date;
     }
 
