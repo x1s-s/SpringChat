85258ea47685a807688fdb64ffaef2f4c2c9f0aa
Aliaksei Shkurko <shaleprog@gmail.com>
2022-10-12 10:24:43 +0300

    delete unused ChatConfig.java

--- src/main/java/by/unie/config/ChatConfig.java
+++
@@ -1,9 +0,0 @@
-package by.unie.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("by.unie.chat")
-public class ChatConfig {
-}
