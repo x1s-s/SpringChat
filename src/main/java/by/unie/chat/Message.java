ac0f03f6328f704342fa95f4c53b4c0b4abc4180
Aliaksei Shkurko <shaleprog@gmail.com>
2022-10-12 10:25:17 +0300

    replace int to long

--- src/main/java/by/unie/chat/Message.java
+++ src/main/java/by/unie/chat/Message.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
     @OneToOne
     @JoinColumn(name = "author_id")
     private User author;
